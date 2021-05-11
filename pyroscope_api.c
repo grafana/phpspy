cce41a44828a866545b0e91fadbae3df605dcffc
kurylak <adrian.kurylak@nokia.com>
2021-05-11 12:33:06 +0200

    static mem alloc for app_cwd, moved some operations to init stage, stack_trace & context are now global

--- pyroscope_api.c
+++ pyroscope_api.c
@@ -1,15 +1,17 @@
 #include "pyroscope_api.h"
+#include <linux/limits.h>
+#include <unistd.h>
 
 /* TODO This is done in the same way in phpspy, so until they fix it, it has to stay like it is */
 #include "phpspy.c"
 
 #define MAX_STACK_DEPTH 50
 
-/* TODO: Will this be shared between all currently debugged php app? I highly doubt!
- * Those would be a separate conteiners, right?
- * But what about phpspy itself and use cases other than pyroscope?*/
+int event_handler(struct trace_context_s *context, int event_type);
+
 typedef struct stack_trace_t {
     trace_frame_t frames[MAX_STACK_DEPTH];
+    char app_root_dir[PATH_MAX];
 } stack_trace_t;
 
 #define handle_error(__rv, __err_ptr, __err_len)                         \
@@ -36,8 +38,16 @@ do{                                                                      \
     }                                                                    \
 } while(0)
 
+struct trace_context_s context;
+stack_trace_t stack_trace;
 int phpspy_init(pid_t pid, void* err_ptr, int err_len) {
     opt_max_stack_depth = MAX_STACK_DEPTH;
+    memset(&stack_trace, 0, sizeof(stack_trace_t));
+    memset(&context, 0, sizeof(struct trace_context_s));
+    context.event_udata = (void*)&stack_trace;
+    context.target.pid = pid;
+    context.event_handler = event_handler;
+    get_process_cwd(&stack_trace.app_root_dir[0], pid);
   return 0;
 }
 
@@ -62,27 +72,20 @@ int event_handler(struct trace_context_s *context, int event_type) {
     return PHPSPY_OK;
 }
 
-void get_process_cwd(char** app_cwd, pid_t pid)
+void get_process_cwd(char* app_cwd, pid_t pid)
 {
-    int app_cwd_len = 0;
     char buf[PATH_MAX];
     snprintf(buf, PATH_MAX, "/proc/%d/cwd", pid);
-    ssize_t alloclen = 512;
-    (*app_cwd) = malloc(alloclen);
-    while ((app_cwd_len = readlink(buf, *app_cwd, alloclen)) == alloclen) {
-        alloclen *= 2;
-        app_cwd = realloc(*app_cwd, alloclen);
-    }
+    int app_cwd_len = readlink(buf, app_cwd, PATH_MAX);
 
     if(app_cwd_len < 0)
     {
-        (*app_cwd)[0] = '\0';
+        app_cwd[0] = '\0';
     }
 }
 
-int parse_output(struct trace_context_s* context, char** app_root_dir, char* data_ptr, int data_len, void* err_ptr, int err_len)
+int parse_output(struct trace_context_s* context, char* app_root_dir, char* data_ptr, int data_len, void* err_ptr, int err_len)
 {
-    /* TODO: Handle lineno == '-1'? or pyroscope shall do it? */
     int written = 0;
     const int nof_frames = context->event.frame.depth;
     for(int current_frame_idx = (nof_frames - 1); current_frame_idx >= 0; current_frame_idx--)
@@ -93,16 +96,15 @@ int parse_output(struct trace_context_s* context, char** app_root_dir, char* dat
         char* write_buffer = ((char*)data_ptr) + written;
 
         int file_path_beginning = 0;
-        if(*app_root_dir)
+        if(app_root_dir[0] != '\0')
         {
-            const int root_path_len = strlen(*app_root_dir);
-            if(memcmp(loc->file, *app_root_dir, root_path_len) == 0)
+            const int root_path_len = strlen(app_root_dir);
+            if(memcmp(loc->file, app_root_dir, root_path_len) == 0)
             {
                 file_path_beginning = root_path_len + 1;
             }
         }
 
-        /* TODO: Optimize, or remove and let pyroscope handle class name */
         if(loc->class_len > 0)
         {
             char out_fmt[] = "%s:%d - %s::%s; ";
@@ -116,11 +118,6 @@ int parse_output(struct trace_context_s* context, char** app_root_dir, char* dat
 
         if(written > data_len)
         {
-            if(*app_root_dir)
-            {
-                free(*app_root_dir);
-                (*app_root_dir) = NULL;
-            }
             int err_msg_len = snprintf((char*)err_ptr, err_len, "Not enough space! %d > %d", written, data_len);
             return -err_msg_len;
         }
@@ -129,24 +126,9 @@ int parse_output(struct trace_context_s* context, char** app_root_dir, char* dat
 }
 
 int phpspy_snapshot(pid_t pid, void* ptr, int len, void* err_ptr, int err_len) {
-    static struct trace_context_s context;
-    static stack_trace_t stack_trace;
-
-    context.event_udata = (void*)&stack_trace;
-    context.target.pid = pid;
-    context.event_handler = event_handler;
-
     handle_error(find_addresses(&context.target), err_ptr, err_len);
     handle_error(do_trace(&context), err_ptr, err_len);
-
-    char* app_root_dir = NULL;
-    /* TODO: get_process_cwd(&app_root_dir, pid); */
-    int written = parse_output(&context, &app_root_dir,  ptr, len, err_ptr, err_len);
-    if(app_root_dir)
-    {
-        free(app_root_dir);
-        app_root_dir = NULL;
-    }
+    int written = parse_output(&context, &stack_trace.app_root_dir[0],  ptr, len, err_ptr, err_len);
 
     return written;
 }
