0ed947ce8ee78a3868aabc93e51ff0d168351c88
adrian.kurylak <adrian.kurylak@gmail.com>
2021-08-03 17:07:57 +0200

    fix?

--- phpspy_trace.c
+++ phpspy_trace.c
@@ -20,7 +20,8 @@ static int sprint_zarray(trace_context_t *context, zend_array *rzarray,
 static int sprint_zarray_bucket(trace_context_t *context, Bucket *lbucket,
                                 char *buf, size_t buf_size, size_t *buf_len);
 
-int do_trace(trace_context_t *context) {
+int do_trace(trace_context_t *context)
+{
   int rv, depth;
   zend_executor_globals executor_globals;
 
@@ -28,26 +29,35 @@ int do_trace(trace_context_t *context) {
   try(rv, context->event_handler(context, PHPSPY_TRACE_EVENT_STACK_BEGIN));
 
   rv = PHPSPY_OK;
-  do {
-#define maybe_break_on_err()                      \
-  do {                                            \
-    if ((rv & PHPSPY_ERR_PID_DEAD) != 0) {        \
-      break;                                      \
-    } else if ((rv & PHPSPY_ERR_BUF_FULL) != 0) { \
-      break;                                      \
-    } else {                                      \
-      break;                                      \
-    }                                             \
+  do
+  {
+#define maybe_break_on_err()                  \
+  do                                          \
+  {                                           \
+    if ((rv & PHPSPY_ERR_PID_DEAD) != 0)      \
+    {                                         \
+      break;                                  \
+    }                                         \
+    else if ((rv & PHPSPY_ERR_BUF_FULL) != 0) \
+    {                                         \
+      break;                                  \
+    }                                         \
+    else                                      \
+    {                                         \
+      break;                                  \
+    }                                         \
   } while (0)
 
     rv |= trace_stack(context, executor_globals.current_execute_data, &depth);
     maybe_break_on_err();
-    if (depth < 1) break;
+    if (depth < 1)
+      break;
 
 #undef maybe_break_on_err
   } while (0);
 
-  if (rv == PHPSPY_OK) {
+  if (rv == PHPSPY_OK)
+  {
     try(rv, context->event_handler(context, PHPSPY_TRACE_EVENT_STACK_END));
   }
 
@@ -55,7 +65,8 @@ int do_trace(trace_context_t *context) {
 }
 
 static int trace_stack(trace_context_t *context,
-                       zend_execute_data *remote_execute_data, int *depth) {
+                       zend_execute_data *remote_execute_data, int *depth)
+{
   int rv;
   zend_execute_data execute_data;
   zend_function zfunc;
@@ -67,7 +78,8 @@ static int trace_stack(trace_context_t *context,
   frame = &context->event.frame;
   *depth = 0;
 
-  while (remote_execute_data && *depth != MAX_STACK_DEPTH) {
+  while (remote_execute_data && *depth != MAX_STACK_DEPTH)
+  {
     memset(&execute_data, 0, sizeof(execute_data));
     memset(&zfunc, 0, sizeof(zfunc));
     memset(&zstring, 0, sizeof(zstring));
@@ -78,30 +90,39 @@ static int trace_stack(trace_context_t *context,
     try_copy_proc_mem("execute_data", remote_execute_data, &execute_data,
                       sizeof(execute_data));
     try_copy_proc_mem("zfunc", execute_data.func, &zfunc, sizeof(zfunc));
-    if (zfunc.common.function_name) {
+    if (zfunc.common.function_name)
+    {
       try(rv, sprint_zstring(context, "function_name",
                              zfunc.common.function_name, frame->loc.func,
                              sizeof(frame->loc.func), &frame->loc.func_len));
-    } else {
+    }
+    else
+    {
       frame->loc.func_len =
           snprintf(frame->loc.func, sizeof(frame->loc.func), "<main>");
     }
-    if (zfunc.common.scope) {
+    if (zfunc.common.scope)
+    {
       try_copy_proc_mem("zce", zfunc.common.scope, &zce, sizeof(zce));
       try(rv,
           sprint_zstring(context, "class_name", zce.name, frame->loc.class_name,
                          sizeof(frame->loc.class_name), &frame->loc.class_len));
-    } else {
+    }
+    else
+    {
       frame->loc.class_name[0] = '\0';
       frame->loc.class_len = 0;
     }
-    if (zfunc.type == 2) {
+    if (zfunc.type == 2 && zfunc.op_array.filename != NULL)
+    {
       try(rv, sprint_zstring(context, "filename", zfunc.op_array.filename,
                              frame->loc.file, sizeof(frame->loc.file),
                              &frame->loc.file_len));
       frame->loc.lineno = zfunc.op_array.line_start;
       /* TODO add comments */
-    } else {
+    }
+    else
+    {
       frame->loc.file_len =
           snprintf(frame->loc.file, sizeof(frame->loc.file), "<internal>");
       frame->loc.lineno = -1;
@@ -116,7 +137,8 @@ static int trace_stack(trace_context_t *context,
 }
 
 static int copy_executor_globals(trace_context_t *context,
-                                 zend_executor_globals *executor_globals) {
+                                 zend_executor_globals *executor_globals)
+{
   int rv;
   executor_globals->current_execute_data = NULL;
   try_copy_proc_mem("executor_globals",
@@ -127,7 +149,8 @@ static int copy_executor_globals(trace_context_t *context,
 
 static int sprint_zstring(trace_context_t *context, const char *what,
                           zend_string *rzstring, char *buf, size_t buf_size,
-                          size_t *buf_len) {
+                          size_t *buf_len)
+{
   int rv;
   zend_string lzstring;
 
@@ -143,37 +166,40 @@ static int sprint_zstring(trace_context_t *context, const char *what,
 }
 
 static int sprint_zval(trace_context_t *context, zval *lzval, char *buf,
-                       size_t buf_size, size_t *buf_len) {
+                       size_t buf_size, size_t *buf_len)
+{
   int rv;
   int type;
   type = (int)lzval->u1.v.type;
-  switch (type) {
-    case IS_LONG:
-      snprintf(buf, buf_size, "%ld", lzval->value.lval);
-      *buf_len = strlen(buf);
-      break;
-    case IS_DOUBLE:
-      snprintf(buf, buf_size, "%f", lzval->value.dval);
-      *buf_len = strlen(buf);
-      break;
-    case IS_STRING:
-      try(rv, sprint_zstring(context, "zval", lzval->value.str, buf, buf_size,
-                             buf_len));
-      break;
-    case IS_ARRAY:
-      try(rv, sprint_zarray(context, lzval->value.arr, buf, buf_size, buf_len));
-      break;
-    default:
-      /* TODO handle other zval types */
-      /* fprintf(context->fout, "value not supported, found type: %d\n", type);
+  switch (type)
+  {
+  case IS_LONG:
+    snprintf(buf, buf_size, "%ld", lzval->value.lval);
+    *buf_len = strlen(buf);
+    break;
+  case IS_DOUBLE:
+    snprintf(buf, buf_size, "%f", lzval->value.dval);
+    *buf_len = strlen(buf);
+    break;
+  case IS_STRING:
+    try(rv, sprint_zstring(context, "zval", lzval->value.str, buf, buf_size,
+                           buf_len));
+    break;
+  case IS_ARRAY:
+    try(rv, sprint_zarray(context, lzval->value.arr, buf, buf_size, buf_len));
+    break;
+  default:
+    /* TODO handle other zval types */
+    /* fprintf(context->fout, "value not supported, found type: %d\n", type);
        */
-      return PHPSPY_ERR;
+    return PHPSPY_ERR;
   }
   return PHPSPY_OK;
 }
 
 static int sprint_zarray(trace_context_t *context, zend_array *rzarray,
-                         char *buf, size_t buf_size, size_t *buf_len) {
+                         char *buf, size_t buf_size, size_t *buf_len)
+{
   int rv;
   int i;
   int array_len;
@@ -189,14 +215,16 @@ static int sprint_zarray(trace_context_t *context, zend_array *rzarray,
   try_copy_proc_mem("buckets", lzarray.arData, buckets,
                     sizeof(Bucket) * array_len);
 
-  for (i = 0; i < array_len; i++) {
+  for (i = 0; i < array_len; i++)
+  {
     try(rv,
         sprint_zarray_bucket(context, buckets + i, buf, buf_size, &tmp_len));
     buf_size -= tmp_len;
     buf += tmp_len;
 
     /* TODO Introduce a string class to clean this silliness up */
-    if (buf_size >= 2) {
+    if (buf_size >= 2)
+    {
       *buf = ',';
       --buf_size;
       ++buf;
@@ -209,7 +237,8 @@ static int sprint_zarray(trace_context_t *context, zend_array *rzarray,
 }
 
 static int sprint_zarray_bucket(trace_context_t *context, Bucket *lbucket,
-                                char *buf, size_t buf_size, size_t *buf_len) {
+                                char *buf, size_t buf_size, size_t *buf_len)
+{
   int rv;
   char tmp_key[PHPSPY_STR_SIZE];
   size_t tmp_len;
@@ -217,12 +246,14 @@ static int sprint_zarray_bucket(trace_context_t *context, Bucket *lbucket,
 
   obuf = buf;
 
-  if (lbucket->key != NULL) {
+  if (lbucket->key != NULL)
+  {
     try(rv, sprint_zstring(context, "array_key", lbucket->key, tmp_key,
                            sizeof(tmp_key), &tmp_len));
 
     /* TODO Introduce a string class to clean this silliness up */
-    if (buf_size > tmp_len + 1 + 1) {
+    if (buf_size > tmp_len + 1 + 1)
+    {
       snprintf(buf, buf_size, "%s=", tmp_key);
       buf_size -= tmp_len + 1;
       buf += tmp_len + 1;
